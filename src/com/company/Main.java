bcd6848a1cdc475eaa908b6bc93639a9cfc23715
Milu-create <koshetok.com@mail.ru>
2020-10-10 17:59:32 +0300

    Верный

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner a = new Scanner(System.in);
-        int n = a.nextInt();
-        int m = a.nextInt();
-        
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int mass[] = new int[a];
+        for(int i = 0; i<a; i++) mass[i] = in.nextInt();
+        int b = in.nextInt();
+        for(int i = 0; i<a; i++){
+            if(b == mass[i]){
+                System.out.println(i + 1);
+                return;
+            }
+        }
+        System.out.println("NO");
     }
 }
